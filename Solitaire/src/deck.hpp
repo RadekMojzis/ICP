26ff2d58fb70ce2b69192e85219191655f7d11f0
fenthrake <john.frezer@gmail.com>
2017-05-06 16:11:15 +0200

    assa
    s

--- Solitaire/src/deck.hpp
+++ Solitaire/src/deck.hpp
@@ -11,18 +11,22 @@ enum DeckID{
     PILE1, PILE2, PILE3, PILE4, PILE5, PILE6, PILE7,
     FOUNDATION1, FOUNDATION2,FOUNDATION3, FOUNDATION4
 };
+typedef unsigned int DeckSize;
+typedef unsigned int ActionID;
 
 class Foundation{
-    private:
+        DeckID id;
         std::vector<Card> cards;
     public:
         Foundation();
         ~Foundation();
+        DeckID getID();
+        bool ActionValidity();
         //const std::vector<Card> & get_cards(){};
 };
 
 class Flip{
-    private:
+        DeckID id;
         std::vector<Card> cards;
     public:
 //        push(Card& card);
@@ -31,25 +35,37 @@ class Flip{
 
         Flip();
         ~Flip();
-        //const std::vector<Card> & get_cards();
+        DeckID getID();
+        bool ActionValidity();
+    //const std::vector<Card> & get_cards();
 };
 
 class Pile{
-    private:
+        DeckID id;
         std::vector<Card> cards;
     public:
 
         Pile() {};
         ~Pile();
-        //const std::vector<Card> & get_cards();
+        DeckID getID();
+        bool ActionValidity();
+    //const std::vector<Card> & get_cards();
 };
 
 
 class Stock{
-    private:
+        DeckID id = 0;
         std::vector<Card> cards;
     public:
+
         Stock() {};
+        Stock(vector<Card> & initCards, int ident): cards{initCards}, id{ident} {};
         ~Stock();
-        //const std::vector<Card> & get_cards();
+        DeckID getID();
+        bool ActionValidity(ActionID action, vector<Card> cards);
+        DeckSize getSize();
+        void addCards(vector<Card> & cards);
+        void removeCards(vector<Card> & cards);
+
+    //const std::vector<Card> & get_cards();
 };
--- Solitaire/src/stock.cpp
+++ Solitaire/src/stock.cpp
@@ -4,3 +4,23 @@
 Card* Stock::get_top(){
     return &(*cards.end());
 }
+
+DeckSize Stock::DeckSize() {
+    return cards.size();
+}
+
+DeckID Stock::getID() {
+    return id;
+}
+
+bool Stock::ActionValidity() {
+
+}
+
+void Stock::addCards(vector <Card> &cards) {
+
+}
+
+void Stock::removeCards(vector <Card> &cards) {
+
+}
