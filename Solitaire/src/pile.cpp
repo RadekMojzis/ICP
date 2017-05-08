19a9b1515ab2b8f5cf65eedb2cbcc62d99dea49a
RadekMojzis <mojzis.radek@centrum.cz>
2017-05-08 04:41:21 +0200

    asdf

--- Solitaire/src/pile.cpp
+++ Solitaire/src/pile.cpp
@@ -8,11 +8,7 @@ std::vector<Card> & Pile::get_cards(){
     std::vector<Card> & ref = cards;
     return ref;
 }
-<<<<<<< HEAD
 
-Pile::Pile(){}
-=======
->>>>>>> origin/master
 
 Pile::~Pile(){}
 
@@ -32,10 +28,6 @@ void Pile::removeCards() {
     cards.pop_back();
 }
 
-<<<<<<< HEAD
-
-=======
 void Pile::push_back(int card){
     cards.push_back(Card(card));
 }
->>>>>>> origin/master
